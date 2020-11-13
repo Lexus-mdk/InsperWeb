c9bfee8766c81c6f2a5b48ed0f6900ac700ad051
LexusPC <dlexusb@gmail.com>
2020-11-13 17:39:45 +0300

    Изменение ветки

--- Сайт/User/iframe.php
+++ Сайт/User/iframe.php
@@ -16,6 +16,7 @@ require '../header.php';
 <iframe id="https://agentpolis.ru/4e3bd263528badfae62199b6ac5fbe7cb4e81dec/eosago#/" scrolling="no" src="https://agentpolis.ru/4e3bd263528badfae62199b6ac5fbe7cb4e81dec/eosago?iframe=agent_widget&amp;bgColor=%23ffffff&amp;titleColor=%23000000&amp;primaryColor=%236A1408&amp;secondaryColor=%231e1913&amp;mainTitle=&amp;prolongAlfa=&amp;prolongIngos=&amp;utm_webmaster=&amp;utm_transitionid=" style="width: 100%; border: none; margin: 0px; padding: 0px;"></iframe>
 </div>
  -->
+ 
 <!-- Соотношение сторон 16:9 -->
 <div class="container">
 <div class="embed-responsive embed-responsive-1by1">
